2e39acdd96b5e91fa83dea8c1d3fa22a6d99a47d
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-08-20 12:48:09 +0600

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Travel App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
