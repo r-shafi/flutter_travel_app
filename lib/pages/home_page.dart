db703959a708b9bae10ae491084bf2464c65a731
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-08-25 11:52:02 +0600

    use state, display real data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_app/configs/colors.dart';
+import 'package:travel_app/cubit/cubits.dart';
+import 'package:travel_app/cubit/states.dart';
+import 'package:travel_app/models/place_model.dart';
 import 'package:travel_app/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,177 +19,199 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     TabController _tabController = TabController(length: 3, vsync: this);
 
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Icon(Icons.menu),
-                  CircleAvatar(
-                    backgroundImage: NetworkImage('https://picsum.photos/200'),
-                    radius: 20,
-                  )
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: AppLargeText(
-                text: 'Discover',
-                letterSpacing: 2,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: 50,
-              child: TabBar(
-                controller: _tabController,
-                labelColor: Colors.black87,
-                unselectedLabelColor: Colors.grey,
-                isScrollable: true,
-                labelPadding:
-                    const EdgeInsets.only(left: 0, bottom: 5, right: 10),
-                indicatorSize: TabBarIndicatorSize.label,
-                indicator:
-                    CircleTabIndicator(color: AppColor.primary, radius: 4),
-                tabs: const [
-                  Tab(
-                    text: 'Places',
+      body: SafeArea(child: BlocBuilder<AppCubits, CubitStates>(
+        builder: (context, state) {
+          if (state is LoadedState) {
+            List<PlaceModel> places = state.places;
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(Icons.menu),
+                      CircleAvatar(
+                        backgroundImage:
+                            NetworkImage('https://picsum.photos/200'),
+                        radius: 20,
+                      )
+                    ],
                   ),
-                  Tab(
-                    text: 'Inspirations',
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: AppLargeText(
+                    text: 'Discover',
+                    letterSpacing: 2,
                   ),
-                  Tab(
-                    text: 'Emotions',
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 20),
-              height: 280,
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 190,
-                        height: 300,
-                        margin: const EdgeInsets.only(right: 20, top: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/mountain.jpeg'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: 5,
-                  ),
-                  ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 190,
-                        height: 300,
-                        margin: const EdgeInsets.only(right: 20, top: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/mountain.jpeg'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: 5,
-                  ),
-                  ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 190,
-                        height: 300,
-                        margin: const EdgeInsets.only(right: 20, top: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/mountain.jpeg'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: 5,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding:
-                  EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  AppLargeText(
-                    text: 'Explore More',
-                    size: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  AppLargeText(
-                    text: 'See All',
-                    size: 14,
-                    color: AppColor.primary,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 100,
-              padding: const EdgeInsets.only(left: 20),
-              child: ListView.builder(
-                itemCount: 10,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 70,
-                        height: 70,
-                        margin: const EdgeInsets.only(right: 20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/hiking.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 50,
+                  child: TabBar(
+                    controller: _tabController,
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    isScrollable: true,
+                    labelPadding:
+                        const EdgeInsets.only(left: 0, bottom: 5, right: 10),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicator:
+                        CircleTabIndicator(color: AppColor.primary, radius: 4),
+                    tabs: const [
+                      Tab(
+                        text: 'Places',
                       ),
-                      const Padding(
-                        padding: EdgeInsets.only(right: 20),
-                        child: Text('Hiking'),
+                      Tab(
+                        text: 'Inspirations',
+                      ),
+                      Tab(
+                        text: 'Emotions',
                       ),
                     ],
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 20),
+                  height: 280,
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              BlocProvider.of<AppCubits>(context)
+                                  .loadDetails(places[index]);
+                            },
+                            child: Container(
+                              width: 190,
+                              height: 300,
+                              margin: const EdgeInsets.only(right: 20, top: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    'http://mark.bslmeiyu.com/uploads/${places[index].img}',
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: places.length,
+                      ),
+                      ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: 190,
+                            height: 300,
+                            margin: const EdgeInsets.only(right: 20, top: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              image: const DecorationImage(
+                                image:
+                                    AssetImage('assets/images/mountain.jpeg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: 5,
+                      ),
+                      ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: 190,
+                            height: 300,
+                            margin: const EdgeInsets.only(right: 20, top: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              image: const DecorationImage(
+                                image:
+                                    AssetImage('assets/images/mountain.jpeg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: 5,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      AppLargeText(
+                        text: 'Explore More',
+                        size: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      AppLargeText(
+                        text: 'See All',
+                        size: 14,
+                        color: AppColor.primary,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  padding: const EdgeInsets.only(left: 20),
+                  child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 70,
+                            height: 70,
+                            margin: const EdgeInsets.only(right: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              image: const DecorationImage(
+                                image: AssetImage('assets/images/hiking.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 20),
+                            child: Text('Hiking'),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      )),
     );
   }
 }
