30d94b53fbae6b247ed569f37b2719b0e92f3ad4
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-08-25 11:50:42 +0600

    state methods

---
+++ lib/cubit/cubits.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:travel_app/cubit/states.dart';
+import 'package:travel_app/models/place_model.dart';
+import 'package:travel_app/services/data_services.dart';
+
+class AppCubits extends Cubit<CubitStates> {
+  AppCubits({
+    required this.dataServices,
+  }) : super(InitialState()) {
+    emit(WelcomeState());
+  }
+
+  final DataServices dataServices;
+  late List<PlaceModel> places;
+
+  void loadPlaces() async {
+    try {
+      emit(LoadingState());
+      places = await dataServices.getPlaces();
+      emit(LoadedState(places));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void loadDetails(PlaceModel place) {
+    emit(DetailsState(place));
+  }
+
+  void goBack() {
+    emit(LoadedState(places));
+  }
+}
